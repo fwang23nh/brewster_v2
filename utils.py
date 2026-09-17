0e914362b0df3390a3a2ca7b1e5de3b604dc9167
Madeline Lam <65207579+madeline-lam@users.noreply.github.com>
2026-09-17 13:41:56 +0100

    Updated max number of pressure layers (#62)
    * added Zhang+2023 profile as ptype=4, untested
    
    * added priors for ptype 4, some values are hardcoded and i dont know where they come from
    
    * Add MultiNest prior support and refactor MCMC prior evaluation
    
    - Introduced/extended MultiNest unit-cube prior transform for user-defined priors
    - Refactored MCMC prior evaluation for clarity and modularity
    - Improved separation between sampling modes (MCMC vs MultiNest)
    - Preserved post-processing physical consistency checks
    
    * update comments for new priors module
    
    * update comments for new priors module
    
    * updated prior values
    
    * Refactored MCMC prior evaluation to support truncated_normal prior setting for ZJ's PT profile
    update utils and priors module for better clarity and modularity
    
    * Rename Priors files
    
    * update Priors.py
    
    * fix a typo
    
    * updated names of parameter intialisation for ptype 4
    
    * updated prior distribution values
    
    * updated prior values
    
    * updated my ptype with truncated_gaussian
    
    * fixed calculation of TP profile, copied PRT implementation
    
    * allow TP profile for ptype 4 to extend beyond 4000K, allowing more flexibility
    
    * updated prior ranges for ptype 4
    
    * made num_coarsePress and num_finePress configurable when making Retrieval_params instance. Setup different default values for ptype=4 and other profile types
    
    * Added tutorial to show how to use profile type 4 with brewster. Also includes an example fitting procedure to use the gradients from a user input TP profile and use those as priors.
    
    * added sonora diamondback pt profile in example data directory
    
    * updated tutorial docs
    
    * increased max number of pressure layers
    
    ---------
    
    Co-authored-by: Madeline Lam <maddie@Madelines-MacBook-Pro-7.local>
    Co-authored-by: Fwang <fwang23nh@gmail.com>
    Co-authored-by: Madeline Lam <maddie@Madelines-MacBook-Pro-8.local>

--- utils.py
+++ utils.py
@@ -565,6 +565,10 @@ class Retrieval_params:
         self.do_fudge = do_fudge
         self.ptype = ptype
 
+        if num_finePress > 1000:
+            raise ValueError(f"Number of user-specified layers greater than maximum number of pressure layers specified\
+                             in sizes_mod.f90 (1000 layers). Either specify less layers or modify the .f90 file and recompile.")
+
         # Set default num_coarsePress and num_finePress depending on chosen profile type
         if self.ptype == 4 and num_coarsePress == None and num_finePress == None:
             self.num_coarsePress = 6
@@ -3100,7 +3104,7 @@ class ArgsGen:
         Generate the required model arguments.
     """
 
-    def __init__(self, re_params, model, instrument, obspec,Mass_priorange=[1.0,80.0],R_priorange=[0.5,2.0], num_coarsePress=13, num_finePress=64):
+    def __init__(self, re_params, model, instrument, obspec,Mass_priorange=[1.0,80.0],R_priorange=[0.5,2.0]):
         self.re_params = re_params
         self.model = model
         self.instrument = instrument
